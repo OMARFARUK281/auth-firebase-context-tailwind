6730a63789c0aec35ec65eff154c43a01f8fead1
OmarFaruk <omarfaruk8431@gmail.com>
2023-04-16 09:52:36 +0600

    tihs is login done

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,36 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+
 import './index.css'
 
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
+import Main from './layout/Main';
+import Home from './components/Header/Home/Home';
+import Login from './components/Login/Login';
+import Register from './components/Rgister/Register';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path: '/login',
+        element: <Login></Login>
+      },
+      {
+        path: '/register',
+        element: <Register></Register>
+      }
+      
+    ]
   },
 ]);
 
